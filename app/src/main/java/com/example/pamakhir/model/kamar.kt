e42ebe89ebb7077e53d6f709aa55bf865bd028f3
Luthfirizaldy <nurachmadluthfi583@gmail.com>
2025-01-26 21:46:23 +0700

    membuat model kamar

---
+++ app/src/main/java/com/example/pamakhir/model/kamar.kt
@@ -0,0 +1,14 @@
+package com.example.pamakhir.model
+
+import kotlinx.serialization.Serializable
+
+
+
+@Serializable
+data class Kamar(
+    val id: String,
+    val nomorKamar: String,
+    val idBangunan : String,
+    val kapasitas: String,
+    val statusKamar: String
+)
