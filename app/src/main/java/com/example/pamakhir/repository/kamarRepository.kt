670c7056ef8aa6e216bdd99c847a6c36fe9e813f
Luthfirizaldy <nurachmadluthfi583@gmail.com>
2025-01-26 22:55:10 +0700

    membuat kamar repository

---
+++ app/src/main/java/com/example/pamakhir/repository/kamarRepository.kt
@@ -0,0 +1,60 @@
+package com.example.pamakhir.repository
+
+import android.util.Log
+import com.example.pamakhir.model.Kamar
+import com.example.pamakhir.service_api.KamarService
+import java.io.IOException
+
+interface KamarRepository{
+    suspend fun getAllKamar() : List<Kamar>
+
+    suspend fun insertKamar(kamar: Kamar)
+
+    suspend fun updateKamar(id : String,kamar: Kamar)
+
+    suspend fun deleteKamar(id:String)
+
+    suspend fun getKamarById(id: String):Kamar
+}
+
+class NetworkKamarRepository(
+    private val kamarApiService : KamarService
+): KamarRepository{
+    override suspend fun getAllKamar(): List<Kamar> =
+        kamarApiService.getAllKamar()
+
+    override suspend fun insertKamar(kamar: Kamar) {
+        kamarApiService.insertKamar(kamar)
+    }
+
+    override suspend fun updateKamar(id: String, kamar: Kamar) {
+        kamarApiService.updateKamar(id,kamar)
+    }
+    override suspend fun deleteKamar(id: String) {
+        try {
+            val response = kamarApiService.deleteKamar(id)
+            if (!response.isSuccessful) {
+                throw IOException(
+                    "Failed to delete Kamar. HTTP Status Code: ${response.code()}"
+                )
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getKamarById(id: String): Kamar {
+        val kamar = kamarApiService.getKamarbyId(id)
+
+        // Menampilkan response JSON di logcat
+        Log.d("MahasiswaAPI", "Response: $kamar")
+
+        return kamar
+    }
+
+
+
+}
