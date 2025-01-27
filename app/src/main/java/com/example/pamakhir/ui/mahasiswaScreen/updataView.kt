c22bfbae6a8802d51f18166e26d3aad036b81e19
Luthfirizaldy <nurachmadluthfi583@gmail.com>
2025-01-27 11:34:07 +0700

    membuat  UI untuk  update di halaman mahasiswa

---
+++ app/src/main/java/com/example/pamakhir/ui/mahasiswaScreen/updataView.kt
@@ -0,0 +1,92 @@
+package com.example.pamakhir.ui.mahasiswaScreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pamakhir.ui.CustomTopAppBar
+import com.example.pamakhir.ui.MahasiswaviewModel.InsertUiEvent
+import com.example.pamakhir.ui.MahasiswaviewModel.PenyediaViewModel
+import com.example.pamakhir.ui.MahasiswaviewModel.UpdateUiState
+import com.example.pamakhir.ui.MahasiswaviewModel.UpdateViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateView(
+    id: String,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    // Muat data mahasiswa berdasarkan ID
+    LaunchedEffect(id) {
+        viewModel.loadingMahasiswaData(id)
+    }
+
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                title = "Update Mahasiswa",
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        UpdateBody(
+            updateUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateMahasiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun UpdateBody(
+    updateUiState: UpdateUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = updateUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
