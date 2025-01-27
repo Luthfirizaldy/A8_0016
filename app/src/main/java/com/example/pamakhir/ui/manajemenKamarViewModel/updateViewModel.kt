0c24d3e149926c65d7db8f8c7b48d62718f2e2d3
Luthfirizaldy <nurachmadluthfi583@gmail.com>
2025-01-27 11:30:07 +0700

    membuat  viewmodel untuk update  di kamar

---
+++ app/src/main/java/com/example/pamakhir/ui/manajemenKamarViewModel/updateViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.pamakhir.ui.manajemenKamarViewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pamakhir.repository.KamarRepository
+import kotlinx.coroutines.launch
+
+class UpdateViewModel(private val kmrRepository: KamarRepository) : ViewModel() {
+
+    var uiStateKamar by mutableStateOf(UpdateUiState())
+        private set
+
+    // Fetch existing Mahasiswa data based on ID and set the state
+    fun loadingKamarData(kamarId: String) {
+        viewModelScope.launch {
+            try {
+                val kamar = kmrRepository.getKamarById(kamarId)
+                uiStateKamar = UpdateUiState(insertUiEvent = kamar.toInsertUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    // Update Mahasiswa data
+    fun updateKamar() {
+        viewModelScope.launch {
+            try {
+                val id = uiStateKamar.insertUiEvent.id
+                val kamar = uiStateKamar.insertUiEvent.toKmr()
+                kmrRepository.updateKamar(id, kamar)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateInsertKmrState(insertUiEvent: InsertUiEvent) {
+        uiStateKamar = UpdateUiState(insertUiEvent = insertUiEvent)
+    }
+}
+
+data class UpdateUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
