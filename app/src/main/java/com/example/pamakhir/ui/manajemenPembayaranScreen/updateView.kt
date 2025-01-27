09ddea8be39a23af8548158625caa7e1274811d4
Luthfirizaldy <nurachmadluthfi583@gmail.com>
2025-01-27 11:40:05 +0700

    membuat  UI untuk  update di halaman pembayaran

---
+++ app/src/main/java/com/example/pamakhir/ui/manajemenPembayaranScreen/updateView.kt
@@ -0,0 +1,93 @@
+package com.example.pamakhir.ui.manajemenKamarScreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pamakhir.ui.CustomTopAppBar
+import com.example.pamakhir.ui.manajemenKamarViewModel.InsertUiEvent
+import com.example.pamakhir.ui.manajemenKamarViewModel.PenyediaViewModel
+import com.example.pamakhir.ui.manajemenKamarViewModel.UpdateUiState
+import com.example.pamakhir.ui.manajemenKamarViewModel.UpdateViewModel
+
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateView(
+    id: String,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    // Muat data kamar berdasarkan ID
+    LaunchedEffect(id) {
+        viewModel.loadingKamarData(id)
+    }
+
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                title = "Update Kamar",
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        UpdateBody(
+            updateUiState = viewModel.uiStateKamar,
+            onKamarValueChange = viewModel::updateInsertKmrState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateKamar()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun UpdateBody(
+    updateUiState: UpdateUiState,
+    onKamarValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = updateUiState.insertUiEvent,
+            onValueChange = onKamarValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
